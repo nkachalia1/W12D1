7eec436c0569f1ebf71831c6654ffac87b48a70d
Neel <nkachalia1@gmail.com>
2023-11-06 12:59:10 -0500

    phase 3

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import { toQueryString } from '../utils';
+import { useEffect } from 'react';
 
-class Weather extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        weather: null
-      };
-    }
-    
-    componentDidMount() {
-      navigator.geolocation?.getCurrentPosition(
-        this.pollWeather,
-        (err) => console.log(err),
-        { timeout: 10000 }
-      );
-    }
+class Weather = () => {
+  const [weather, setWeather] = useState(null);
+  useEffect( () => {
+    console.log(err),
+      { timeout: 10000 }
+  })
+}
+
 
     pollWeather = async (location) => {
       let url = 'http://api.openweathermap.org/data/2.5/weather?';
@@ -29,14 +23,14 @@ class Weather extends React.Component {
       "process.env.<variable_name>". Make sure to .gitignore your .env file!
       Also remember to restart your server (i.e., re-run "npm start") whenever
       you change your .env file. */
-      const apiKey = '???';
+      const apiKey = process.env.REACT_APP_WEATHER_API
 
       const params = {
         lat: location.coords.latitude,
         lon: location.coords.longitude,
         appid: apiKey
       };
-      
+
       url += toQueryString(params);
 
       const res = await fetch(url);
@@ -52,7 +46,7 @@ class Weather extends React.Component {
   render() {
     const weather = this.state.weather;
     let content = <div className='loading'>loading weather...</div>;
-    
+
     if (weather) {
       const temp = (weather.main.temp - 273.15) * 1.8 + 32;
       content = (
@@ -65,7 +59,7 @@ class Weather extends React.Component {
     else {
       content = (
         <div>
-          Weather is currently unavailable. (Are Location Services enabled?) 
+          Weather is currently unavailable. (Are Location Services enabled?)
         </div>
       )
     }
